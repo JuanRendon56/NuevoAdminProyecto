ed88499443423f2a4038f02b9ae08389ece9fd7e
JuanRendon56 <A01731524@itesm.mx>
2021-11-29 14:03:06 -0600

    Primera parte LOGIN

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
+import { FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule, 
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRouting } from './routes/routing';
-
+import { LoginComponent } from './components/login/login.component';
+//Importacion y aplicacion de todos los modulos
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    AppRouting
+    AppRouting,
+    FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/routes/routing.ts
+++ front-end/src/app/routes/routing.ts
@@ -1,9 +1,12 @@
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "../components/home/home.component";
+import { LoginComponent } from "../components/login/login.component";
 
 const app_routes:Routes=[
     { path:'home', component:HomeComponent},
+    { path:'login', component:LoginComponent},
     { path:'**', pathMatch:'full', redirectTo:''}
 ]
 
 export const AppRouting=RouterModule.forRoot(app_routes);
+//Servicio de routing y pathing
