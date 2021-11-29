6f03ba5e27f3517b787775610846c7334a39e3db
JuanRendon56 <A01731524@itesm.mx>
2021-11-29 12:51:38 -0600

    Routing ejecutando

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRouting } from './routes/routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
